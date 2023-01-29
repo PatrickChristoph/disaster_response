90e2ba402c14317c5cbb3486ff6f2f1171bbc53d
PatrickChristoph <pchristoph87@googlemail.com>
2023-01-29 12:16:25 +0100

    added loading and cleaning to ETL process

--- data/process_data.py
+++ data/process_data.py
@@ -1,25 +1,59 @@
 import sys
 
-
-def load_data(messages_filepath, categories_filepath):
-    pass
-
-
-def clean_data(df):
-    pass
+import pandas as pd
+from sqlalchemy import create_engine
 
 
-def save_data(df, database_filename):
-    pass  
+def load_data(messages_filepath: str, categories_filepath: str) -> pd.DataFrame:
+    """
+    Load and merge disaster messages and disaster categories input data.
 
+    :param messages_filepath: path of messages csv file
+    :param categories_filepath: path of categories csv file
+    :return: loaded disaster input data
+    """
+    messages = pd.read_csv('disaster_messages.csv')
+    categories = pd.read_csv('disaster_categories.csv')
+
+    return messages.merge(categories, how='outer', on='id')
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transform data in correct format and remove unusable data.
+
+    :param df: messages with assigned categories
+    :return: cleaned data
+    """
+    # transform data into appropriate columns
+    categories = df['categories'].str.split(';', expand=True)
+    category_columns = categories.iloc[0].apply(lambda x: x[:-2])
+    categories.columns = category_columns
+
+    for column in categories:
+        categories[column] = categories[column].str[-1:]
+        categories[column] = categories[column].astype(int)
+
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # remove duplicates and unusable messages
+    df.drop_duplicates(subset=['id'], inplace=True)
+    df = df[~df["message"].str.len() < 15]
+
+    return df
+
+def save_data(df: pd.DataFrame, database_filename: str) -> None:
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('data', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        messages_filepath = 'disaster_categories.csv'
+        categories_filepath = 'disaster_messages.csv'
+        database_filepath = 'etl.db'
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print('Loading data...')
         df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
